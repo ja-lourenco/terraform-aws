73ebb5a91531030c6eb3b7600ef2ca802fd3144a
João Antonio <ja.lourenco@outlook.com>
2026-04-09 08:07:19 -0300

    feat: add project name variable

--- vpc.tf
+++ vpc.tf
@@ -3,5 +3,5 @@ resource "aws_vpc" "eks_vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = merge(local.tags, { Name = "comunidadedevops-vpc" })
+  tags = merge(local.tags, { Name = "${var.project_name}-vpc" })
 }

67573489213af0653cf81f0e6d4a460d494804b2
João Antonio <ja.lourenco@outlook.com>
2026-04-24 22:56:49 -0300

    feat: add load balancer controller service account

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -9,3 +9,11 @@ output "oidc_url" {
 output "oidc_arn" {
   value = local.oidc_arn
 }
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "cluster_ca_cert" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
--- provider.tf
+++ provider.tf
@@ -4,9 +4,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.39.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.1.0"
+    }
   }
 }
 
 provider "aws" {
   region = local.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_ca_cert)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
